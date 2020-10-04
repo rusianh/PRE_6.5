fc4c0aa9e1dd5f9463caa57f96c0e7e07312bdb2
VietAnhVu <vva1994@gmail.com>
2020-10-05 00:05:48 +0700

    lan 2

---
+++ 6.5.js
@@ -0,0 +1,19 @@
+/**
+ * CHƯƠNG TRÌNH JavaScript QUẢN LÝ MẢNG
+ *
+ * JavaScript không hỗ trợ lệnh goto. Vì thế nên dùng true/false
+ * của choice để làm cho chương trình bị lặp.
+ *@created: 2017.09.24
+ *@update: 2020.09.25
+ */
+
+
+ var list = [];
+
+ function main()
+ {
+     var choise = false;
+
+     do
+     
+ }
